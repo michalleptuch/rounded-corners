e95a47847ebba92b07b74a5a9b9fc41c794bde9a
Michał Łeptuch <michal.leptuch@outlook.com>
2020-03-13 20:43:07 +0100

    Implementation

---
+++ RoundedCorners/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace RoundedCorners
+{
+  public class Program
+  {
+    [System.STAThread()]
+    public static void Main()
+    {
+      using (new Uwp.App())
+      {
+        App app = new App();
+        app.InitializeComponent();
+        app.Run();
+      }
+    }
+  }
+}
